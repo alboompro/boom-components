f74dcaa4d76c3661110aa556cd12aab0eff20619
César <costa.cesar.a@hotmail.com>
2019-01-14 10:07:18 -0200

    docs(Input): update docs

--- src/components/data-entry/input/Input.js
+++ src/components/data-entry/input/Input.js
@@ -12,8 +12,50 @@ import { DefaultInput } from './styles'
 const Input = ({ size, ...props }) => <DefaultInput size={size} {...props} />
 
 Input.propTypes = {
+  /** label text or component displayed on the right side of the input */
+  addonAfter: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** label text or component displayed on the left side of the input */
+  addonBefore: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** whether input content can be removed with clear icon */
+  clearable: PropTypes.bool,
+  /** initial input content */
+  defaultValue: PropTypes.string,
+  /** disabled state of the input */
+  disabled: PropTypes.string,
   /** callback when value changes */
   onChange: PropTypes.func,
+  /** callback when enter key is pressed */
+  onPressEnter: PropTypes.func,
+  /** prefix icon inside input */
+  prefix: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** suffix icon inside input */
+  suffix: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** HTML type of input */
+  type: PropTypes.oneOf([
+    'button',
+    'checkbox',
+    'color',
+    'date',
+    'datetime',
+    'datetime-local',
+    'email',
+    'file',
+    'hidden',
+    'image',
+    'month',
+    'number',
+    'password',
+    'radio',
+    'range',
+    'reset',
+    'search',
+    'submit',
+    'tel',
+    'text',
+    'time',
+    'url',
+    'week'
+  ]),
   /** size of the input box */
   size: PropTypes.oneOf(['small', 'default', 'large']),
   /** input content value */
@@ -21,7 +63,9 @@ Input.propTypes = {
 }
 
 Input.defaultProps = {
-  size: 'default'
+  disabled: false,
+  size: 'default',
+  type: 'text'
 }
 
 export default Input
