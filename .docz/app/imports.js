928b8623baa1e4f3e90fbf099117f78cc7349866
César <costa.cesar.a@hotmail.com>
2019-01-14 10:08:43 -0200

    docs(Radio): update docs

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,6 +1,10 @@
 export const imports = {
-  'src/components/data-entry/input/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-input-index" */ 'src/components/data-entry/input/index.mdx'),
   'src/components/general/button/index.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-general-button-index" */ 'src/components/general/button/index.mdx'),
+  'src/components/data-entry/checkbox/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-checkbox-index" */ 'src/components/data-entry/checkbox/index.mdx'),
+  'src/components/data-entry/input/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-input-index" */ 'src/components/data-entry/input/index.mdx'),
+  'src/components/data-entry/radio/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-radio-index" */ 'src/components/data-entry/radio/index.mdx'),
 }
---
+++ src/components/data-entry/radio/Radio.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Radio = ({ ...props }) => <div />
+
+Radio.propTypes = {
+  /** whether radio is selected */
+  checked: PropTypes.bool,
+  /** specifies the initial state of radio */
+  defaultChecked: PropTypes.bool,
+  /** disabled status of radio */
+  disabled: PropTypes.bool,
+  /** for comparison, to determine whether is the selected  */
+  value: PropTypes.any
+}
+
+Radio.defaultProps = {
+  checked: false,
+  defaultChecked: false,
+  disabled: false,
+  indeterminate: false
+}
+
+export default Radio
