a96883b2b0223687956e3fd787df04058c66fc13
César <costa.cesar.a@hotmail.com>
2019-01-15 11:05:11 -0200

    docs(Dropdown): add docs

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -5,6 +5,8 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-autocomplete-index" */ 'src/components/data-entry/autocomplete/index.mdx'),
   'src/components/data-entry/checkbox/index.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-checkbox-index" */ 'src/components/data-entry/checkbox/index.mdx'),
+  'src/components/data-entry/dropdown/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-dropdown-index" */ 'src/components/data-entry/dropdown/index.mdx'),
   'src/components/data-entry/input/index.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-data-entry-input-index" */ 'src/components/data-entry/input/index.mdx'),
   'src/components/data-entry/radio/index.mdx': () =>
