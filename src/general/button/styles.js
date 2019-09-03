289858e0d3b750d1948184f8173ea86d123d7f9c
JohnnErick <jon91052@gmail.com>
2019-09-03 20:47:47 +0000

    feat(button loading): create button loading and loading component

--- src/general/button/styles.js
+++ src/general/button/styles.js
@@ -2,33 +2,80 @@ import styled from "styled-components";
 
 import colors from "../../constants/colors";
 
-export const DefaultBtn = styled.button`
-  display: inline-block;
+export const Icon = styled.span`
+  svg {
+    min-height: 10px;
+    height: 100%;
+  }
+`;
+
+export const LoadingFeedback = styled.div`
+  position: relative;
+  height: 100%;
+  opacity: 0;
+  transition: 0.3s;
+
+  > span {
+    position: absolute;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const ButtonContent = styled.div`
+  display: flex;
   align-items: center;
   justify-content: center;
-  height: 35px;
-  padding: 0 20px;
+  font-size: 14px;
+`;
+
+export const ButtonChildren = styled.div`
+  font-size: 14px;
+  transition: 0.3s;
+
+  ${props => props.textStyle}
+`;
+
+export const DefaultBtn = styled.button`
+  display: flex;
+  position: relative;
+  padding: 0 25px;
   border: 0;
   border-radius: 2px;
-  font-size: 16px;
   cursor: pointer;
-  transition: opacity 150ms ease-in-out;
   background-color: ${props => colors[props.colorType] || colors.default};
-  color: ${props => (props.colorType === "primary" ? "#fff" : "#000")};
+  transition: opacity 150ms ease-in-out;
+
+  ${ButtonContent} {
+    height: 35px;
+    color: ${props => (props.colorType === "primary" ? "#fff" : "#000")};
+  }
+
+  &:focus {
+    outline: none;
+  }
 
   &:hover {
     opacity: 0.8;
   }
 
   &:disabled {
-    cursor: no-drop;
+    // cursor: no-drop;
     opacity: 0.5;
   }
-`;
 
-export const Icon = styled.span`
-  svg {
-    min-height: 10px;
-    height: 100%;
+  &.loading {
+    opacity: 0.5;
+
+    ${LoadingFeedback} {
+      opacity: 1;
+      transform: translate(-${props => props.loadingSize / 2 + 2}px);
+    }
+
+    ${ButtonChildren} {
+      transform: translate(${props => props.loadingSize / 2 + 2}px);
+    }
   }
 `;
---
+++ src/shared/loader/Loader.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Loader = ({ loadingSize = 20, loadingColor = "#B5B5B5" }) => (
+  <svg
+    width={loadingSize}
+    height={loadingSize}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    preserveAspectRatio="xMidYMid"
+  >
+    <circle
+      cx="50"
+      cy="50"
+      fill="none"
+      stroke={loadingColor}
+      strokeWidth="10"
+      r="35"
+      strokeDasharray="164.93361431346415 56.97787143782138"
+      transform="rotate(215.545 50 50)"
+    >
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        calcMode="linear"
+        values="0 50 50;360 50 50"
+        keyTimes="0;1"
+        dur="1s"
+        begin="0s"
+        repeatCount="indefinite"
+      />
+    </circle>
+  </svg>
+);
+
+export default Loader;
