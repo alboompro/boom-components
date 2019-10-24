67e93a396427d597ec0fa936a2a66d0305174706
Robson Formigão <robson.formigao.gomes@hotmail.com>
2019-10-24 14:13:19 +0000

    Improv/modal change styles

--- src/data-display/modal/styles.js
+++ src/data-display/modal/styles.js
@@ -9,16 +9,16 @@ export const ModalBackdrop = styled.div`
   width: 100vw;
   max-width: 100%;
   height: 100vh;
-  z-index: ${props => props.zIndex}; 
-  
+  z-index: ${props => props.zIndex};
+
   ${props => props.backdrop && "background-color: rgba(129, 129, 129, 0.8);"}
   ${props => props.backdrop && props.backdropStyle}
-  ${props => props.floatingStyle.top
-    ? 'align-items: flex-start;'
-    : props.floatingStyle.bottom 
-    ? 'align-items: flex-end;'
-    : 'align-items: center;'
-  }
+  ${props =>
+    props.floatingStyle.top
+      ? "align-items: flex-start;"
+      : props.floatingStyle.bottom
+      ? "align-items: flex-end;"
+      : "align-items: center;"}
 `;
 
 export const ModalContainer = styled.div`
@@ -27,18 +27,18 @@ export const ModalContainer = styled.div`
   width: ${props =>
     typeof props.width === "string" ? props.width : `${props.width}px`};
   height: ${props =>
-    typeof props.height  === "string" ? props.height : `${props.height}px`};
+    typeof props.height === "string" ? props.height : `${props.height}px`};
   max-width: 90%;
   max-height: 90%;
   background-color: ${props => props.background};
   border-radius: ${props => (props.rounded ? "3px" : 0)};
-  
-  ${props => props.floatingStyle.top 
-    ?  `margin-top: ${props.floatingStyle.top };`
-    : props.floatingStyle.bottom 
-    ? `margin-bottom: ${props.floatingStyle.bottom };`
-    : null
-  }
+
+  ${props =>
+    props.floatingStyle.top
+      ? `margin-top: ${props.floatingStyle.top};`
+      : props.floatingStyle.bottom
+      ? `margin-bottom: ${props.floatingStyle.bottom};`
+      : null}
 
   ${props => props.styles}
 `;
@@ -50,12 +50,23 @@ export const ModalHeader = styled.div`
   justify-content: center;
   padding: 18px;
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.26);
+  z-index: 1;
 `;
 
 export const CloseIcon = styled.div`
   cursor: pointer;
   margin-left: auto;
-  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 3px;
+  padding: 10px;
+  transition: all 0.1s ease-out 0s;
+
+  &:hover {
+    background-color: #efefef;
+  }
+
   > img {
     width: 16px;
     pointer-events: none;
@@ -69,6 +80,7 @@ export const ModalTitle = styled.div`
   letter-spacing: 0.4px;
   line-height: initial;
   font-weight: 500;
+  text-transform: uppercase;
 `;
 
 export const ModalBody = styled.div`
