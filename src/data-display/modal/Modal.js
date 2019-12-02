3863ac51ecfae580e39d7dec0befa008d6777dba
Robson Formigão <rformigao.gomes@gmail.com>
2019-12-02 18:09:04 -0300

    fix(Modal): change when overflow is setted to null

--- src/data-display/modal/Modal.js
+++ src/data-display/modal/Modal.js
@@ -28,7 +28,6 @@ class Modal extends Component {
   closeModal = e => {
     const { onClose, backdropClosable } = this.props;
     const { backdrop, closeButton } = this.refs;
-    document.body.style.overflow = null;
 
     if (
       backdropClosable &&
@@ -36,6 +35,7 @@ class Modal extends Component {
         e.target ===
           (closeButton.refs ? closeButton.refs.closeButton : closeButton))
     ) {
+      document.body.style.overflow = null;
       onClose();
     }
   };
