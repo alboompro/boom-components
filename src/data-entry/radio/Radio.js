ee7816ae95206dbd25cf171f6c00c89fde0b0d67
Robson Formigão <rformigao.gomes@gmail.com>
2019-12-11 14:41:41 -0300

    feat(Radio): create component radio

--- src/data-entry/radio/Radio.js
+++ src/data-entry/radio/Radio.js
@@ -1,24 +1,67 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Radio = ({ ...props }) => <div />;
+import { noop } from "../../helpers";
+
+import { Label, RadioButton } from "./styles";
+
+const Radio = ({
+  id,
+  name,
+  value,
+  defaultChecked,
+  label,
+  onChange,
+  disabled,
+  className,
+  style
+}) => (
+  <Label
+    htmlFor={id}
+    onChange={onChange}
+    disabled={disabled}
+    className={className}
+    style={style}
+  >
+    <RadioButton
+      disabled={disabled}
+      type="radio"
+      id={id}
+      name={name}
+      value={value}
+      defaultChecked={defaultChecked}
+    />
+    {label}
+  </Label>
+);
 
 Radio.propTypes = {
-  /** whether radio is selected */
-  checked: PropTypes.bool,
+  /** add custom className */
+  className: PropTypes.string,
   /** specifies the initial state of radio */
   defaultChecked: PropTypes.bool,
-  /** disabled status of radio */
+  /** disabled click of radio */
   disabled: PropTypes.bool,
-  /** for comparison, to determine whether is the selected  */
-  value: PropTypes.any
+  /** specifies id of radio */
+  id: PropTypes.string.isRequired,
+  /** label of radio */
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  /** name of radio group */
+  name: PropTypes.string.isRequired,
+  /** callback when click radio */
+  onChange: PropTypes.func,
+  /** component main style */
+  style: PropTypes.object,
+  /** value of radio when selected */
+  value: PropTypes.string.isRequired
 };
 
 Radio.defaultProps = {
-  checked: false,
+  className: "",
   defaultChecked: false,
   disabled: false,
-  value: null
+  onChange: noop,
+  style: null
 };
 
 export default Radio;
