367a972dd37b9476a2521b9e90391188e90e0aaa
Robson Formigão <rformigao.gomes@gmail.com>
2020-01-28 11:26:53 -0300

    improv(Notice): add min-width to notification

--- src/feedback/notification/styles.js
+++ src/feedback/notification/styles.js
@@ -2,19 +2,32 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  align-items: ${props => (props.title ? "flex-start" : "center")};
   background-color: #fff;
   box-shadow: 0 0px 8px rgba(0, 0, 0, 0.3);
   margin-bottom: 16px;
   padding: 15px 25px;
   overflow: hidden;
   border-radius: 4px;
+  min-width: 180px;
   max-width: 270px;
   position: relative;
   transition: all 0.3s ease-out;
+  font-size: 16px;
+  line-height: initial;
   opacity: ${props => (props.visible ? 1 : 0)};
   transform: ${props =>
     props.visible ? "translateX(0px)" : "translateX(335px)"};
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  margin-top: -3px;
+  font-size: 16px;
 `;
 
 export const Message = styled.span`
@@ -22,6 +35,12 @@ export const Message = styled.span`
 `;
 
 export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+`;
+
+export const IconCloseContainer = styled.div`
   position: absolute;
   right: 16px;
   cursor: pointer;
