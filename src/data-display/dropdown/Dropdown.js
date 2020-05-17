afe402e566d2d8c850c0f3993111cd52abff249a
Welington Sampaio <webmaster.lelo@gmail.com>
2020-05-17 17:57:06 -0300

    feat(DropDown): implement top position
    based in position of trigger in relation with the screen
    when in 90% or more the contents appear in top of element

--- src/data-display/dropdown/Dropdown.js
+++ src/data-display/dropdown/Dropdown.js
@@ -45,7 +45,6 @@ class Dropdown extends Component {
   };
 
   clickOnDocumentHandler = e => {
-    const v = this.clickToHide(e);
     if (this.clickToHide(e)) {
       e && e.preventDefault();
       this.update(false);
@@ -117,19 +116,25 @@ class Dropdown extends Component {
       right,
       left,
       width,
+      height,
       bottom
     } = e.currentTarget.getBoundingClientRect();
+
+    const toTop = bottom / window.innerHeight > 0.9;
+
     const placement = {
-      top: window.scrollY + bottom
+      toTop,
+      top: window.scrollY - (toTop ? height : 0) + bottom,
+      transform: toTop ? "translateY(calc(-100% - 8px))" : ""
     };
     switch (align) {
       case "right":
         placement.left = right;
-        placement.transform = "translateX(-100%)";
+        placement.transform += "translateX(-100%)";
         break;
       case "center":
         placement.left = left + width / 2;
-        placement.transform = "translateX(-50%)";
+        placement.transform += "translateX(-50%)";
         break;
       default:
         placement.left = left;
@@ -191,7 +196,11 @@ class Dropdown extends Component {
                 ref={this.setContainerRef}
                 style={placement}
               >
-                <DropdownContent align={align} style={overlayStyle}>
+                <DropdownContent
+                  align={align}
+                  style={overlayStyle}
+                  arrowPos={placement.toTop ? "top" : "bottom"}
+                >
                   {overlay}
                 </DropdownContent>
               </DropdownPlacement>
