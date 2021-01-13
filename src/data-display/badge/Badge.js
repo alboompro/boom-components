be24d598c0558bcc658fd600e44f1dbe5973ddd5
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-01-13 15:49:48 -0300

    feat/badge - background type added

--- src/data-display/badge/Badge.js
+++ src/data-display/badge/Badge.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 const Badge = ({
   children,
   color,
+  type,
   background,
   padding,
   borderRadius,
@@ -12,27 +13,42 @@ const Badge = ({
   display,
   pos,
   ...props
-}) => (
-  <div
-    style={{
-      color,
-      backgroundColor: background,
-      padding,
-      borderRadius,
-      fontSize,
-      textAlign,
-      display,
-      position: pos.position,
-      top: pos.top,
-      right: pos.right,
-      bottom: pos.bottom,
-      left: pos.left
-    }}
-    {...props}
-  >
-    {children}
-  </div>
-);
+}) => {
+  function setBackgroundColor() {
+    if (!type) {
+      return background;
+    }
+    if (type === "success") {
+      return "#52C41A";
+    }
+    if (type === "error") {
+      return "#FF4D4F";
+    }
+    return "#44ADF8";
+  }
+
+  return (
+    <div
+      style={{
+        color,
+        backgroundColor: setBackgroundColor(),
+        padding,
+        borderRadius,
+        fontSize,
+        textAlign,
+        display,
+        position: pos.position,
+        top: pos.top,
+        right: pos.right,
+        bottom: pos.bottom,
+        left: pos.left
+      }}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
 
 Badge.propTypes = {
   /** defines the text of the badge */
@@ -40,8 +56,10 @@ Badge.propTypes = {
     .isRequired,
   /** defines color of the text, can be hex or text */
   color: PropTypes.string.isRequired,
-  /** defines the background color of the badge */
-  background: PropTypes.string.isRequired,
+  /** defines the background color based on the type passed */
+  type: PropTypes.oneOf(["success", "error", "default", null]),
+  /** defines the background color of the badge as a custom color (without the type) */
+  background: PropTypes.string,
   /** set a space around the badge content, must contain the measure, example: "10px", "5px 10px 15px 20px" */
   padding: PropTypes.string,
   /** rounds the badge border, must contain the measure, example: "10px", "50%"" */
@@ -50,6 +68,7 @@ Badge.propTypes = {
   fontSize: PropTypes.string,
   /** defines the text alignment in the badge */
   textAlign: PropTypes.oneOf(["left", "center", "right", "justify"]),
+  /** sets the badge style display */
   display: PropTypes.oneOf(["inline", "inline-block", "block", "none"]),
   /** customize the positioning of the Badge, it must contain the measures, example: "10px" */
   pos: PropTypes.shape({
@@ -62,8 +81,10 @@ Badge.propTypes = {
 };
 
 Badge.defaultProps = {
+  type: null,
+  background: "unset",
   padding: "unset",
-  borderRadius: "unset",
+  borderRadius: "8px",
   fontSize: "1rem",
   textAlign: "center",
   display: "inline",
