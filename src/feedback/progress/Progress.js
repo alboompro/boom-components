526a3621c3b118ebe2ac74902b16bc3b364dbb8c
kszinhu <rodrigues.cassiano2007@gmail.com>
2021-08-09 13:04:43 -0300

    initial addition of some props

--- src/feedback/progress/Progress.js
+++ src/feedback/progress/Progress.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { noop } from "../../helpers";
 
@@ -10,17 +10,50 @@ import {
   ProgressText
 } from "./styles.js";
 
-const Progress = ({ min, total, current, text, textType }) => {
+const Progress = ({
+  min,
+  total,
+  current,
+  showIcon,
+  showText,
+  unitMeasurement,
+  styleProgress,
+  color,
+  ...props
+}) => {
+  const [status, setStatus] = React.useState(0);
+
   const progressProps = {
     min,
     max: total,
-    value: current
+    value: current,
+    color,
+    styleProgress
   };
 
+  const textProps = {
+    unitMeasurement,
+    styleText: props.styleText
+  };
+
+  useEffect(() => {
+    if (current >= total) {
+      // If the progress is 100%
+      setStatus(2);
+    } else if (current > min && current < total) {
+      // If the progress is not 100%
+      setStatus(1);
+    } else if (current == min) {
+      // If the progress is 0%
+      setStatus(0);
+    }
+  }, [current, total, min]);
+
   const renderLabel = () =>
-    text && (
-      <ProgressText>
-        <span>{`${current}${textType}`}</span>
+    showText && (
+      <ProgressText {...textProps}>
+        {/* To do: Adicionar renderização de um ícone */}
+        <span>{`${current}${unitMeasurement}`}</span>
       </ProgressText>
     );
 
@@ -39,31 +72,37 @@ const Progress = ({ min, total, current, text, textType }) => {
 Progress.propTypes = {
   /** Minimum value of the progress bar */
   min: PropTypes.number,
-  /** The total progress */
+  /** Total value for the progress bar */
   total: PropTypes.number,
-  /** The current progress */
+  /** Current value of the progress bar */
   current: PropTypes.number,
-  /** The current progress bar color */
+  /** Current progress bar color */
   color: PropTypes.string,
   /** The template function of the content */
   template: PropTypes.func,
-  /** Show/Hide Text of progress bar */
-  text: PropTypes.bool,
-  /** Type of Text of progress bar */
-  textType: PropTypes.string,
-  /** Style of progress bar */
-  style: PropTypes.object
+  /** Show/Hide icon on progress bar label */
+  showIcon: PropTypes.bool,
+  /** Show/Hide text on progress bar label */
+  showText: PropTypes.bool,
+  /** Measuring unit used on progress bar label */
+  unitMeasurement: PropTypes.string,
+  /** Style object for progress bar */
+  styleProgress: PropTypes.object,
+  /** Style object for progress bar label */
+  styleText: PropTypes.object
 };
 
 Progress.defaultProps = {
   min: 0,
   total: 100,
   current: 0,
-  color: "",
+  color: "royalblue",
   template: noop,
-  text: false,
-  textType: "%",
-  style: {}
+  showIcon: false,
+  showText: false,
+  unitMeasurement: "%",
+  styleProgress: {},
+  styleText: {}
 };
 
 export default Progress;
--- src/feedback/progress/styles.js
+++ src/feedback/progress/styles.js
@@ -18,21 +18,26 @@ export const Bar = styled.div`
   vertical-align: middle;
   width: 100%;
   overflow: hidden;
-  background-color: #f5f5f5; // To do: add color prop
   border-radius: 100px;
+
+  background-color: #f5f5f5; // To do: add color prop
 `;
 
 export const ProgressBar = styled.div`
   position: relative;
   transition: all 0.5s cubic-bezier(0.08, 0.82, 0.17, 1);
   border-radius: 100px;
-  background-color: blueviolet; // To do: Mudar a cor com o estilo
 
-  height: 8px; // To do: mudar o tamanho do progress bar
+  background-color: ${props => props.color};
   width: ${props => props.value}%;
+
+  height: 8px;
 `;
 
 export const ProgressText = styled.div`
-  margin-left: 8px;
-  font-weight: bold;
+  ${props =>
+    props.textStyle
+      ? props.textStyle
+      : `margin-left: 8px;
+        font-weight: bold;`};
 `;
