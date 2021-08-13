63a5812a4aeb86f85b52a01dd8edc1360c9cb4d9
Nathan D Saggioro <nathan.saggioro@alboompro.com>
2021-08-13 15:49:23 -0300

    Test fix based on actual component logic

--- src/data-display/breadcrumb/Breadcrumb.test.js
+++ src/data-display/breadcrumb/Breadcrumb.test.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { mount } from "enzyme";
 import Breadcrumb from "./Breadcrumb";
+import Icon from '../../general/icon'
 
 test('Shows a breadcrumb', () => {
-    const selected = crumb => {console.log(crumb)};
     const crumbs = [
         {
-            name: 'Home', 
-            link: '#'
+            customNode: <a href="#"> Home </a>
         },
         {
-            name: 'Category', 
-            link: '#'
+            name: 'Busca', 
+            link: '#',
+            icon: <Icon kind="bold" group="arrows-diagrams" category="arrows" file="arrow-button-right.svg" size="10" color="#000000" />,
         },
         {
-            name: 'Sub-Category', 
-            link: '#'
+            name: 'Nuvem', 
+            link: '#',
+            separator: "/"
         }
     ];
 
-    const wrapper = mount(<Breadcrumb crumbs={crumbs} selected={selected} />);
-    expect(wrapper.text()).toMatch('Home','Category','Sub-Category');
+    const wrapper = mount(<Breadcrumb crumbs={crumbs} />);
+    expect(wrapper.text()).toMatch('Home','Busca','Nuvem');
   });
